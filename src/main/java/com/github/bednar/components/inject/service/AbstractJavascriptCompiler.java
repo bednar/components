e9eb39c6c2b1b1527bb85814a18f8afbea0deaa4
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-12-29 18:16:01 +0100

    * Prepare for Jade compiler

---
+++ src/main/java/com/github/bednar/components/inject/service/AbstractJavascriptCompiler.java
@@ -0,0 +1,87 @@
+package com.github.bednar.components.inject.service;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+import com.github.bednar.base.utils.resource.FluentResource;
+import com.github.bednar.base.utils.throwable.FluentException;
+import org.apache.commons.lang3.time.StopWatch;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.tools.shell.Global;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Jakub Bednář (29/12/2013 18:02)
+ */
+public abstract class AbstractJavascriptCompiler
+{
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractJavascriptCompiler.class);
+
+    private final Context context;
+    private final Scriptable scope;
+
+    public AbstractJavascriptCompiler(@Nonnull String... scriptPaths)
+    {
+        context = Context.enter();
+        context.setLanguageVersion(Context.VERSION_1_8);
+        context.setOptimizationLevel(-1);
+
+        Global global = new Global();
+        global.init(context);
+
+        scope = context.initStandardObjects(global);
+
+        for (String scriptPath : scriptPaths)
+        {
+            load(scriptPath);
+        }
+    }
+
+    @Nonnull
+    protected String evaluateInline(@Nonnull final String name, @Nonnull final String script)
+    {
+        StopWatch watch = new StopWatch();
+        watch.start();
+
+        LOG.info("[evaluating...][{}]", name);
+
+        try
+        {
+            Context context = Context.enter();
+            context.setLanguageVersion(Context.VERSION_1_8);
+
+            return context.evaluateString(scope, script, "compile-inline.js", 1, null).toString();
+        }
+        finally
+        {
+            watch.stop();
+
+            LOG.info("[evaluated][{}][{}]", name, watch.toString());
+
+            Context.exit();
+        }
+    }
+
+    private void load(@Nonnull final String scriptPath)
+    {
+        StopWatch watch = new StopWatch();
+        watch.start();
+
+        try (FluentResource script = FluentResource.byPath(scriptPath))
+        {
+            LOG.info("[loading-script...][{}]", script.path());
+
+            context.evaluateReader(scope, script.asReader(), scriptPath, 1, null);
+
+            watch.stop();
+
+            LOG.info("[loaded-script][{}][{}]", script.path(), watch.toString());
+        }
+        catch (IOException e)
+        {
+            throw FluentException.internal(e);
+        }
+    }
+}
--- src/main/java/com/github/bednar/components/inject/service/LessCssCompilerImpl.java
+++ src/main/java/com/github/bednar/components/inject/service/LessCssCompilerImpl.java
@@ -1,42 +1,18 @@
 package com.github.bednar.components.inject.service;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
 import java.net.URL;
 
 import com.github.bednar.base.utils.resource.FluentResource;
-import com.github.bednar.base.utils.throwable.FluentException;
-import org.apache.commons.lang3.time.StopWatch;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.tools.shell.Global;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Jakub Bednář (29/12/2013 11:32)
  */
-public class LessCssCompilerImpl implements LessCssCompiler
+public class LessCssCompilerImpl extends AbstractJavascriptCompiler implements LessCssCompiler
 {
-    private static final Logger LOG = LoggerFactory.getLogger(LessCssCompilerImpl.class);
-
-    private final Context context;
-    private final Scriptable scope;
-
     public LessCssCompilerImpl()
     {
-        context = Context.enter();
-        context.setLanguageVersion(Context.VERSION_1_8);
-        context.setOptimizationLevel(-1);
-
-        Global global = new Global();
-        global.init(context);
-
-        scope = context.initStandardObjects(global);
-
-        load("/compiler/env.rhino.1.2.js");
-        load("/compiler/less/less-1.5.0.min.js");
-        load("/compiler/less/compileLess.js");
+        super("/compiler/env.rhino.1.2.js", "/compiler/less/less-1.5.0.min.js", "/compiler/less/compileLess.js");
     }
 
     @Override
@@ -61,44 +37,11 @@ public class LessCssCompilerImpl implements LessCssCompiler
     @Nonnull
     private String compile(@Nonnull final FluentResource lessResource, @Nonnull final Boolean compress)
     {
-        String lessPath     = lessResource.path();
-        String lessContent  = lessResource.asString().replaceAll("\\s", " ");
+        String lessPath = lessResource.path();
+        String lessContent = lessResource.asString().replaceAll("\\s", " ");
 
         String script = String.format("compileLess('%s', '%s', %s);", lessPath, lessContent, compress);
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-
-        LOG.info("[compile...][{}]", lessPath);
-
-        try
-        {
-            Context context = Context.enter();
-            context.setLanguageVersion(Context.VERSION_1_8);
-
-            return context.evaluateString(scope, script, "compile-inline.js", 1, null).toString();
-        }
-        finally
-        {
-            stopWatch.stop();
-
-            LOG.info("[compiled][{}][{}]", lessPath, stopWatch.toString());
-
-            Context.exit();
-        }
-    }
-
-    private void load(@Nonnull final String scriptPath)
-    {
-        try (FluentResource script = FluentResource.byPath(scriptPath))
-        {
-            LOG.info("[load-script][{}]", script.path());
-
-            context.evaluateReader(scope, script.asReader(), scriptPath, 1, null);
-        }
-        catch (IOException e)
-        {
-            throw FluentException.internal(e);
-        }
+        return evaluateInline(lessPath, script);
     }
 }
